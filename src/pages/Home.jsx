e32dfbf1e8fe672c7d5a86ada15a4d153ae04e4e
RandyCheBro <kolyavolya@yandex.ru>
2024-01-05 04:04:58 +0300

    добавляет адаптивность на главной странице

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,9 +26,10 @@ function Home() {
 
   React.useEffect(() => {
     getItems();
+    window.scrollTo(0, 0)
   }, []);
   return (
-      <>
+      <div className="container">
         <div className="content__top">
           <Categories />
           <Sort />
@@ -39,7 +40,7 @@ function Home() {
             ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
             : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
         </div>
-      </>
+      </div>
   );
 }
 
